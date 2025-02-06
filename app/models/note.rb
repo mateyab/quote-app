3af7c6e0b56d5ea8318ad1348c67d8f1febe481f
mateyab <matey.bsm@gmail.com>
2025-02-06 15:49:39 -0800

    adding broadcasting with turbo streams

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,8 @@
 class Note < ApplicationRecord
   validates :name, presence: true
   scope :ordering, -> { order(id: :desc) }
-
+  after_create_commit -> {broadcast_prepend_to "notes", partial: "notes/note", locals: {note: self}, target: "notes"}
+  #execute every time a note is added to the database
+  after_update_commit -> { broadcast_replace_to "notes"} #this is the same as above because of rails convention
+  after_destroy_commit -> {broadcast_remove_to "notes"}
 end
